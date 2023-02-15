e9c8ef62e7000cbe1ecc1df9206f642bced37059
Dmitriy <dmitriytsua@gmail.com>
2023-02-15 23:15:28 +0200

    Small fix

--- js/exercise4.js
+++ js/exercise4.js
@@ -3,11 +3,6 @@
 
 let userDriverCategory = prompt('Enter a driver category A, B or C(wrong letter or number will be replaced to A category): ', 'A').toUpperCase();
 
-for (let userCategory of userDriverCategory){
-userDriverCategory+=userCategory
-}
-
-
 switch (userDriverCategory.replace(/[D-Z0-9 ]/g, '')[0]) {
     case 'A':
         document.write('You are motorcycle driver')
