2b2c56f403d63c2917a2d494b6c8a8776c42de64
Dmitriy <dmitriytsua@gmail.com>
2023-02-16 02:49:15 +0200

    Small fix

--- js/exercise4.js
+++ js/exercise4.js
@@ -1,7 +1,7 @@
 // З клавіатури вводиться назва категорії водія (А-мотоцикл, В-легковий автомобіль, С-вантажний автомобіль).
 // Вивести на екран назву транспортного засобу, яким він може керувати.
 
-let userDriverCategory = prompt('Enter a driver category A, B or C(wrong letter or number will be replaced to A category): ', 'A').toUpperCase();
+let userDriverCategory = prompt('Enter a driver category A, B or C: ', 'A').toUpperCase();
 
 switch (userDriverCategory.replace(/[D-Z0-9 ]/g, '')[0]) {
     case 'A':
